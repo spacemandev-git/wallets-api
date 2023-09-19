56238f6e6ec603805555ae94ab9e9f047ba87310
spacemandev <devbharel@gmail.com>
2023-09-19 00:25:24 -0500

    pushing changes

--- src/processor.ts
+++ src/processor.ts
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 dotenv.config();
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Wallets } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const CARPOOL_QUERY_URL = "https://mainnet.carpool.dev/query/solana";
@@ -29,15 +29,26 @@ async function updateActiveWallets() {
 
   // Get Top Wallets in BGUM
   try {
+    let wallets: Wallets[] = (await getBGUMAddresses()).map((v) => {
+      return {
+        wallet: v.value,
+        bgumMint: 0,
+        bgumTransfer: 0,
+        bgumBurn: 0,
+        tensorIX: 0,
+        meIX: 0,
+        sharkyIX: 0,
+        foxyIX: 0,
+        cmMints: 0,
+        sol: 0,
+      };
+    });
+    wallets = await processBGUMMintBatch(wallets);
+    wallets = await processBGUMTransferBatch(wallets);
+    wallets = await processBGUMBurnBatch(wallets);
+
     await prisma.wallets.createMany({
-      data: (
-        await getBGUMAddresses()
-      ).map((v) => {
-        return {
-          wallet: v.value,
-          tokens: {},
-        };
-      }),
+      data: wallets,
     });
   } catch (e) {
     console.error(e);
@@ -73,4 +84,228 @@ async function getBGUMAddresses() {
     throw new Error(bgumTopAddresses.error);
   }
   return bgumTopAddresses.response.values;
+}
+
+async function processBGUMMintBatch(wallets: Wallets[]) {
+  let newWalletArray: Wallets[] = [];
+  for (let i = 0; i <= 1000; i += 10) {
+    newWalletArray.push(
+      ...(await Promise.all(
+        wallets.slice(i, i + 10).map(async (wallet) => {
+          const response = (await (
+            await fetch(CARPOOL_QUERY_URL, {
+              headers: {
+                "x-api-key": process.env.CARPOOL_KEY!,
+                "Content-Type": "application/json",
+              },
+              method: "post",
+              body: JSON.stringify({
+                type: "MOST_COMMON_ACCOUNTS_INSTRUCTION",
+                query: {
+                  programId: "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY",
+                  accountName: "leafOwner",
+                  filter: {
+                    or: [
+                      {
+                        type: "INSTRUCTION_NAME",
+                        instructionName: "mintToCollectionV1",
+                      },
+                      {
+                        type: "INSTRUCTION_NAME",
+                        instructionName: "mintV1",
+                      },
+                    ],
+                    and: {
+                      type: "INSTRUCTION_ACCOUNT",
+                      accountPubKey: wallet.wallet,
+                      accountName: "leafOwner",
+                    },
+                  },
+                  sort: {
+                    order: "desc",
+                  },
+                  timeRange: {
+                    after: thirtyDaysAgo,
+                  },
+                  limit: 1,
+                },
+              }),
+            })
+          ).json()) as any;
+          if (response.error) {
+            throw new Error(response.error);
+          } else {
+            wallet.bgumMint = response.response.values[0]?.count || 0;
+          }
+          return wallet;
+        })
+      ))
+    );
+  }
+  return newWalletArray;
+}
+
+async function processBGUMTransferBatch(wallets: Wallets[]) {
+  let newWalletArray: Wallets[] = [];
+  for (let i = 0; i <= 1000; i += 10) {
+    newWalletArray.push(
+      ...(await Promise.all(
+        wallets.slice(i, i + 10).map(async (wallet) => {
+          const response = (await (
+            await fetch(CARPOOL_QUERY_URL, {
+              headers: {
+                "x-api-key": process.env.CARPOOL_KEY!,
+                "Content-Type": "application/json",
+              },
+              method: "post",
+              body: JSON.stringify({
+                type: "MOST_COMMON_ACCOUNTS_INSTRUCTION",
+                query: {
+                  programId: "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY",
+                  accountName: "leafOwner",
+                  filter: {
+                    or: [
+                      {
+                        type: "INSTRUCTION_NAME",
+                        instructionName: "transfer",
+                      },
+                    ],
+                    and: {
+                      type: "INSTRUCTION_ACCOUNT",
+                      accountPubKey: wallet.wallet,
+                      accountName: "leafOwner",
+                    },
+                  },
+                  sort: {
+                    order: "desc",
+                  },
+                  timeRange: {
+                    after: thirtyDaysAgo,
+                  },
+                  limit: 1,
+                },
+              }),
+            })
+          ).json()) as any;
+          if (response.error) {
+            throw new Error(response.error);
+          } else {
+            wallet.bgumTransfer = response.response.values[0]?.count || 0;
+          }
+          return wallet;
+        })
+      ))
+    );
+  }
+  return newWalletArray;
+}
+
+async function processBGUMBurnBatch(wallets: Wallets[]) {
+  let newWalletArray: Wallets[] = [];
+  for (let i = 0; i <= 1000; i += 10) {
+    newWalletArray.push(
+      ...(await Promise.all(
+        wallets.slice(i, i + 10).map(async (wallet) => {
+          const response = (await (
+            await fetch(CARPOOL_QUERY_URL, {
+              headers: {
+                "x-api-key": process.env.CARPOOL_KEY!,
+                "Content-Type": "application/json",
+              },
+              method: "post",
+              body: JSON.stringify({
+                type: "MOST_COMMON_ACCOUNTS_INSTRUCTION",
+                query: {
+                  programId: "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY",
+                  accountName: "leafOwner",
+                  filter: {
+                    or: [
+                      {
+                        type: "INSTRUCTION_NAME",
+                        instructionName: "burn",
+                      },
+                    ],
+                    and: {
+                      type: "INSTRUCTION_ACCOUNT",
+                      accountPubKey: wallet.wallet,
+                      accountName: "leafOwner",
+                    },
+                  },
+                  sort: {
+                    order: "desc",
+                  },
+                  timeRange: {
+                    after: thirtyDaysAgo,
+                  },
+                  limit: 1,
+                },
+              }),
+            })
+          ).json()) as any;
+          if (response.error) {
+            throw new Error(response.error);
+          } else {
+            wallet.bgumBurn = response.response.values[0]?.count || 0;
+          }
+          return wallet;
+        })
+      ))
+    );
+  }
+  return newWalletArray;
+}
+
+async function processTensorIXBatch(wallets: Wallets[]) {
+  let newWalletArray: Wallets[] = [];
+  for (let i = 0; i <= 1000; i += 10) {
+    newWalletArray.push(
+      ...(await Promise.all(
+        wallets.slice(i, i + 10).map(async (wallet) => {
+          const response = (await (
+            await fetch(CARPOOL_QUERY_URL, {
+              headers: {
+                "x-api-key": process.env.CARPOOL_KEY!,
+                "Content-Type": "application/json",
+              },
+              method: "post",
+              body: JSON.stringify({
+                type: "MOST_COMMON_ACCOUNTS_INSTRUCTION",
+                query: {
+                  programId: "TSWAPaqyCSx2KABk68Shruf4rp7CxcNi8hAsbdwmHbN",
+                  accountName: "owner",
+                  filter: {
+                    or: [
+                      {
+                        type: "INSTRUCTION_NAME",
+                        instructionName: "burn",
+                      },
+                    ],
+                    and: {
+                      type: "INSTRUCTION_ACCOUNT",
+                      accountPubKey: wallet.wallet,
+                      accountName: "leafOwner",
+                    },
+                  },
+                  sort: {
+                    order: "desc",
+                  },
+                  timeRange: {
+                    after: thirtyDaysAgo,
+                  },
+                  limit: 1,
+                },
+              }),
+            })
+          ).json()) as any;
+          if (response.error) {
+            throw new Error(response.error);
+          } else {
+            wallet.bgumBurn = response.response.values[0]?.count || 0;
+          }
+          return wallet;
+        })
+      ))
+    );
+  }
+  return newWalletArray;
 }
